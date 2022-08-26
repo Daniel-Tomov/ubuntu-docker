c184d8470e9700ee5ec1dee3f85216d98b2a0db1
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2022-08-26 15:02:21 -0400

    separate build

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-docker build --tag danielubuntu .
 docker run -d -P -i -t danielubuntu
 docker exec $(docker ps | grep -vwE "(CONTAINER ID|ubuntu)" | awk '{print $1}') service ssh restart
 docker ps
